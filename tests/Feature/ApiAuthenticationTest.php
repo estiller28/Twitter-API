48573fe8633abd4a5ec451c17a2725590aa7a385
Daniel Estiller <estillerdaniel@gmail.com>
2023-07-05 01:57:27 +0800

    installed Passport for API authentication

--- tests/Feature/ApiAuthenticationTest.php
+++ tests/Feature/ApiAuthenticationTest.php
@@ -13,7 +13,7 @@ class ApiAuthenticationTest extends TestCase
      */
     public function test_example(): void
     {
-        $response = $this->get('/');
+        $response = $this->get(route('register'));
 
         $response->assertStatus(200);
     }
